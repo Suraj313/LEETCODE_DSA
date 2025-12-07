1aa3ca0d84d12d036f59805a86610b236f637b0f
Suraj Singh <144615470+Suraj313@users.noreply.github.com>
2025-12-07 11:29:22 +0530

    Time: 11 ms (26.87%) | Memory: 18.3 MB (7.77%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,29 +1,42 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int n=s.size();
-        map<char,int>m={
-            {'I',1},
-            {'V',5},
-            {'X',10},
-            {'L',50},
-            {'C',100},
-            {'D',500},
-            {'M',1000}
-        };
-        int ans=0;
-        for(int i=n-1;i>=0;i--){
-            if(i==n-1){
-                ans+=m[s[i]];
-            }
-            else{
-                if(m[s[i]]>=m[s[i+1]]){
-                    ans+=m[s[i]];
+        unordered_map<string,int> m;
+        m["I"]  = 1;
+        m["V"]  = 5;
+        m["X"]  = 10;
+        m["L"]  = 50;
+        m["C"]  = 100;
+        m["D"]  = 500;
+        m["M"]  = 1000;
+        m["IV"] = 4;
+        m["IX"] = 9;
+        m["XL"] = 40;
+        m["XC"] = 90;
+        m["CD"] = 400;
+        m["CM"] = 900;
+
+        int ans = 0;
+        int i = 0;
+
+        while (i < s.size()) {
+
+            if (i < s.size() - 1) {
+                string sub2 = s.substr(i, 2);   
+                auto it2 = m.find(sub2);
+                if (it2 != m.end()) {
+                    ans += it2->second;
+                    i += 2;                     
+                    continue;
                 }
-                else ans-=m[s[i]];
             }
+
+            string sub1 = s.substr(i, 1);
+            auto it1 = m.find(sub1);
+            ans += it1->second;
+            i += 1;                             
         }
-        
-    return ans;    
+
+        return ans;
     }
 };
